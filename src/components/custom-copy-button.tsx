aa1be373b50657c74b90e23ce5a425b50c55abc7
Mārtiņš Trubiņš <35902174+trubinsh@users.noreply.github.com>
2026-02-18 00:11:20 +0200

    Merge pull request #23
    * #13 uuid generator layout

--- src/components/custom-copy-button.tsx
+++ src/components/custom-copy-button.tsx
@@ -1,11 +1,11 @@
 import {ActionIcon, CopyButton, Tooltip} from "@mantine/core";
 import {IconCheck, IconCopy} from "@tabler/icons-react";
 
-function CustomCopyButton({value, timeout} : {value: string, timeout?: number}) {
+function CustomCopyButton({value, timeout, tooltip} : {value: string, timeout?: number, tooltip?: string}) {
   return (
     <CopyButton value={value} timeout={timeout ?? 2000}>
       {({copied, copy}) => (
-        <Tooltip label={copied ? 'Copied' : 'Copy'} withArrow
+        <Tooltip label={copied ? 'Copied' : tooltip ?? 'Copy'} withArrow
                  position="right">
           <ActionIcon color={copied ? 'teal' : 'gray'} variant="light"
                       onClick={copy} aria-label="Copy password">
